4df3f379528e5a57e156e5fd3cbfbf61405830f5
Mattia Florio <55547136+Mattia921@users.noreply.github.com>
2022-01-20 19:44:37 +0100

    docker-github error

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,7 @@ from alembic import command
 
 #SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Mattia92@localhost:5432/fastapi_test'
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)  #it estabilished a connection to the DB
 
