edf6ad422ecd8b6da17167e4ef5c57eff2d09c70
Mattia Florio <55547136+Mattia921@users.noreply.github.com>
2022-01-27 17:52:55 +0100

    hard coding .env problems

--- app/config.py
+++ app/config.py
@@ -1,7 +1,6 @@
 from pydantic import BaseSettings
-from dotenv import load_dotenv
 
-
+"""
 class Settings(BaseSettings):
     database_hostname: str
     database_port: str
@@ -14,6 +13,18 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+"""
 
-load_dotenv()
+class Settings(BaseSettings):
+    database_hostname = "localhost"
+    database_port = "5432"
+    database_password = "Mattia92"
+    database_name = "fastapi"
+    database_username = "postgres"
+    secret_key = "f646ce0b45db3e9dec6ab24a47c7a480f56624e5065edced4e921ab815cfa77b"
+    algorithm = "HS256"
+    access_token_expire_minutes = 60
+
+
+
 settings = Settings()
